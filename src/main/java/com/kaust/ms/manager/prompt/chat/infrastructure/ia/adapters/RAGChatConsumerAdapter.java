e41315c58e2827f58e5106d7a29db1244b2e12e8
Willians Quispe <37058101+CodeTecnologyYT@users.noreply.github.com>
2025-11-19 17:48:16 -0500

    Feature/add vector grap (#7)
    * Integrate RAG API support for chat handling, including updated model and creativity configurations, improved markdown processing, and enhanced report generation with Flexmark HTML rendering.
    
    * Integrate RAG API support for chat handling, including updated model and creativity configurations, improved markdown processing, and enhanced report generation with Flexmark HTML rendering.
    
    * Refactor chat response handling to use `ChatMessageResponse`, update repository default behavior for missing settings, and remove obsolete `@NotEmpty` validation from quantityCreativity fields.
    
    * feat: Add `ERROR_API_BIOMEDICAL_CONNEXION` exception and handle biomedical API connection errors in `RAGChatConsumerAdapter`.

--- src/main/java/com/kaust/ms/manager/prompt/chat/infrastructure/ia/adapters/RAGChatConsumerAdapter.java
+++ src/main/java/com/kaust/ms/manager/prompt/chat/infrastructure/ia/adapters/RAGChatConsumerAdapter.java
@@ -3,8 +3,11 @@ package com.kaust.ms.manager.prompt.chat.infrastructure.ia.adapters;
 import com.kaust.ms.manager.prompt.chat.domain.ports.RAGChatConsumerApiPort;
 import com.kaust.ms.manager.prompt.chat.infrastructure.ia.model.BiomedicalResponse;
 import com.kaust.ms.manager.prompt.chat.infrastructure.mappers.ToBiomedicalRequest;
+import com.kaust.ms.manager.prompt.shared.exceptions.ManagerPromptError;
+import com.kaust.ms.manager.prompt.shared.exceptions.ManagerPromptException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
@@ -31,6 +34,10 @@ public class RAGChatConsumerAdapter implements RAGChatConsumerApiPort {
                 .uri(urlBase + "/chat")
                 .bodyValue(request)
                 .retrieve()
+                .onStatus(HttpStatus.INTERNAL_SERVER_ERROR::equals, response ->
+                        response.bodyToMono(String.class)
+                                .flatMap(body -> Mono.error(new ManagerPromptException(ManagerPromptError.ERROR_API_BIOMEDICAL_CONNEXION, HttpStatus.INTERNAL_SERVER_ERROR.value())))
+                )
                 .bodyToMono(BiomedicalResponse.class);
     }
 
